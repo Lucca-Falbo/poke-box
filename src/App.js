d5a79f0a1f06e70ebedefd28207c01cac4033c00
Lucca-Falbo <luccafalbo@gmail.com>
2021-04-02 18:10:33 -0300

    first components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function GetPokemonList() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setPokemons(result.results);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  return pokemons;
+}
+
+function PokemonList() {
+  let pokemons = GetPokemonList();
+  let listOfPokemons = {};
+  let counter = 1;
+  pokemons.forEach((pokemon) => {
+    listOfPokemons[pokemon.name] = {
+      name: pokemon.name,
+      id: counter,
+      sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${counter}.png`,
+    };
+    counter++;
+  });
+  return (
+    <div className="container">
+      {Object.values(listOfPokemons).map((pokemon) => (
+        <div className="pokemonContainer" key={pokemon.id}>
+          {pokemon.name}
+          <img src={pokemon.sprite}></img>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Temos que pegar</h1>
+      <PokemonList />
     </div>
   );
 }
